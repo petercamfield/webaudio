18c021bbfcf84fb8e4599245d6c4f046c12fe8c0
Peter Camfield <peter@camfield.net>
2018-05-30 12:05:43 +0100

    Simple sine wave

---
+++ sine.js
@@ -0,0 +1,9 @@
+(
+  const context = new (window.AudioContext || window.webkitAudioContext)();
+  const osc = context.createOscillator();
+  osc.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+  osc.frequency.value = 2500; // value in hertz
+  osc.connect(context.destination);
+  osc.start();
+  window.setTimeout(osc.stop, 1000);
+)
