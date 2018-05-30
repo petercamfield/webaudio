5ff1680116b0e5f93bc7980e28a1e41dc511ecac
Peter Camfield <peter.camfield@scribestar.com>
2018-05-30 12:37:02 +0100

    Stop the sound

--- sine.js
+++ sine.js
@@ -1,9 +1,8 @@
 
-  var context = new (window.AudioContext || window.webkitAudioContext)();
-  var osc = context.createOscillator();
+  const context = new (window.AudioContext || window.webkitAudioContext)();
+  const osc = context.createOscillator();
   osc.type = 'sine'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
   osc.frequency.value = 2500; // value in hertz
   osc.connect(context.destination);
   osc.start();
-  window.setTimeout(osc.stop, 1000);
-
+  osc.stop(context.currentTime + 1);
