c17d243ee690f4c754477032f0992d94ad604783
Peter Camfield <peter.camfield@scribestar.com>
2018-06-06 21:36:47 +0100

    Play through channel on click

--- sine.js
+++ sine.js
@@ -1,16 +1,27 @@
+
+document.getElementById('play').onclick=()=>{
   const context = new (window.AudioContext || window.webkitAudioContext)();
   const osc = context.createOscillator();
   osc.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
   osc.frequency.value = 2500; // value in hertz
-  const gain = context.createGain();
-   
-
-
-
-
-  osc.connect(gain);
-  gain.connect(context.destination);
+  const c1 = createMixerChannel(context);
+  osc.connect(c1).connect(context.destination);
   osc.start();
-  gain.gain.exponentialRampToValueAtTime(1.0, context.currentTime + 1);
-  gain.gain.exponentialRampToValueAtTime(0.01, context.currentTime + 2);
-  osc.stop(context.currentTime + 2.5);
+  osc.stop(context.currentTime + 1.5);
+};
+
+function createMixerChannel(context) {
+  // insert -> gain -> compressor -> eq -> fx -> pan -> fader
+  const gain = context.createGain();
+  const compressor = context.createDynamicsCompressor();
+  const eqHi = context.createBiquadFilter();
+  const eqMid = context.createBiquadFilter();
+  const eqLo = context.createBiquadFilter();
+  const pan = context.createStereoPanner();
+  const fader = context.createGain();
+  eqLo.type = "lowshelf";
+  eqHi.type = "highshelf";
+  eqMid.type = "peaking";
+  return gain.connect(compressor).connect(eqHi).connect(eqMid).connect(eqLo).connect(pan).connect(fader);
+}
+
